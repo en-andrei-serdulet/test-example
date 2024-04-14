a3adf4e0e6762a8cb497b6494b7b17ba2671f516
Andrei Serdulet <Andrei.Serdulet@endava.com>
2024-04-14 19:50:08 +0300

    test plugin

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //hello world
+const Copy = () => {
+    const metadata = {
+        kind: 'execute'
+    }
+
+    const execute = async (inputs) => {
+        console.log('inputs', inputs)
+    }
+
+    return {
+        metadata,
+        execute
+    }
+}
+
+exports.Copy = Copy
